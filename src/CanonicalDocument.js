4383c24e4712a787e773824732db717aca19f9ea
Mark Howe <mvahowe@gmail.com>
2021-08-04 15:46:13 +0200

    Correct sequenceId selector

--- src/CanonicalDocument.js
+++ src/CanonicalDocument.js
@@ -12,7 +12,8 @@ class CanonicalDocument extends ScriptureParaDocument {
 const addActions = (dInstance) => {
 
     const selectedSequence = (renderer, context) =>
-        !renderer.config.sequenceId || context.sequenceStack[0].id === renderer.config.sequenceId;
+        (!renderer.config.sequenceId && context.sequenceStack[0].type === 'main') ||
+        context.sequenceStack[0].id === renderer.config.sequenceId;
 
     dInstance.addAction(
         'startDocument',
